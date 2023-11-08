92ca12eb813798d8b9c514b352cfdb77923d3144
Larissa Gobbo <larissafgobbo@icloud.com>
2023-11-08 18:49:21 -0300

    Projeto em JavaScript finalizado com sucesso!

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+// variável usada para determinar o nome e o nível do meu herói (poderia ser usada uma constante, porém, preferi a variavel)
+let nomeDoHeroi = "O Heroi do JavaScript" // pode ser modificado para o nome do seu héroi.
+let xpDoHeroi = 7001 // pode ser modificado para o nível desejado.
+
+// utilizado uma estrutura de decisão para informar e determinar o nível do heroí
+ 
+if(xpDoHeroi < 1000){
+   console.log(nomeDoHeroi + ", está no nível ferro.") 
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000){
+    console.log(nomeDoHeroi + ", está no nível bronze.")
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000){
+    console.log(nomeDoHeroi + ", está no nível prata.")
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000){
+    console.log(nomeDoHeroi + ", está no nível ouro.")
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000){
+    console.log(nomeDoHeroi + ", está no nível platina.")
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000){
+    console.log(nomeDoHeroi + ", está no nível ascendente")
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 1000){
+    console.log(nomeDoHeroi + ", está no nível imortal.")
+} else (radiante)
+
+
